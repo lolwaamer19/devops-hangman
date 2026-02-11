32029bf4150144634f3c9c6bcd1b6a3df707df15
Lolwa Alhemaidi <60105155@udst.edu.qa.com>
2026-02-11 10:14:51 +0300

    Fix delete word to remove selected word (fixes #5)

--- script.js
+++ script.js
@@ -130,11 +130,14 @@ function editWord(index) {
 }
 
 function deleteWord(index) {
-    if (confirm('Are you sure you want to delete this word?')) {
+    if (confirm("Are you sure you want to delete this word?")) {
+        wordBank.splice(index, 1);
         saveWordBank();
         displayWordBank();
+        
     }
 }
+
 
 function generateKeyboard() {
     const keyboard = document.getElementById('keyboard');
